3fd0760598947f8e731a3debb2f50d330be10561
Michal <michael.wlodarczak@gmail.com>
2021-07-24 13:46:52 +0200

    8th commit. improve removeProductIntoStorage method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,18 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Product product1 = new Product("1","apple",3.5,ProductCategory.GROCERIES);
-        Product product2 = new Product("2","Domestos",15.5,ProductCategory.CHEMICALS);
+        Product product1 = new Product("1", "apple", 3.5, ProductCategory.GROCERIES);
+        Product product2 = new Product("2", "Domestos", 15.5, ProductCategory.CHEMICALS);
 
         Storage storage = new Storage();
-        storage.putProductIntoStorage(product1,5);
-        storage.putProductIntoStorage(product2,10);
+        storage.putProductIntoStorage(product1, 5);
+        storage.putProductIntoStorage(product2, 10);
         storage.productStorage.forEach((k, v) -> System.out.println(k + " " + v));
-        storage.putProductIntoStorage(product2,5);
+        storage.putProductIntoStorage(product2, 5);
         storage.productStorage.forEach((k, v) -> System.out.println(k + " " + v));
-
+        storage.removeProductFromStorage(product1, 5);
+        storage.productStorage.forEach((k, v) -> System.out.println(k + " " + v));
+        storage.removeProductFromStorage(product1, 1);
+        storage.productStorage.forEach((k, v) -> System.out.println(k + " " + v));
     }
 }
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -7,20 +7,24 @@ public class Storage implements RecordOfGoods {
     @Override
     public void putProductIntoStorage(Product product, int quantity) {
         if (productStorage.keySet().contains(product)) {
-                productStorage.put(product, productStorage.get(product) + quantity);
-            } else {
-                productStorage.put(product, quantity);
-            }
+            productStorage.put(product, productStorage.get(product) + quantity);
+        } else {
+            productStorage.put(product, quantity);
+        }
         System.out.println(productStorage.size());
     }
 
     @Override
     public void removeProductFromStorage(Product product, int quantity) {
-        for (Product product1 : productStorage.keySet())
-            if (product1.equals(product)){
-                productStorage.remove(product,productStorage.get(product)-quantity);
-            }else {
-                System.out.println("No product inside storage");
+        if (productStorage.keySet().contains(product)) {
+            if (productStorage.get(product) - quantity < 0) {
+                System.out.println("Not enough items inside storage. Available products inside storage: " + productStorage.get(product));
+            } else {
+                productStorage.put(product, productStorage.get(product) - quantity);
             }
+        } else {
+            System.out.println("No product inside storage");
+        }
+        System.out.println(productStorage.size());
     }
 }
