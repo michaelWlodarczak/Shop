89513ab242d6b0148ad9abd8cc7cbffbacd1fc2b
Michal <michael.wlodarczak@gmail.com>
2021-07-24 11:00:52 +0200

    3rd commit. add enum 'ProductCategory'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,27 @@
-public class Main {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> products = new HashMap<>();
+        products.put("jabłko", "2");
+        products.put("gruszka", "6");
+        products.put("winogrona","10");
+        products.put("cytryna","15");
+
+
+        // iterowanie po kluczach
+        for (String names : products.keySet()) {
+            System.out.println(names);
+        }
+        // iterowanie po wartosciach
+        for (String job : products.values()) {
+            System.out.println(job);
+        }
+        // iterowanie po parach
+        for (Map.Entry<String, String> pair: products.entrySet()){
+            System.out.println("Produkt: " + pair.getKey() + "; Ilosc " + pair.getValue());
+        }
+        System.out.println(products);
+    }
 }
---
+++ src/main/java/ProductCategory.java
@@ -0,0 +1,3 @@
+public enum ProductCategory {
+    GROCERIES, CHEMICALS, ELECTRONICS,
+}
