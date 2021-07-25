51de48b16585d0725ea925fa65dd0c3715edab55
Michal <michael.wlodarczak@gmail.com>
2021-07-25 12:40:55 +0200

    12th commit. displayStorage method

--- src/main/java/CartsMethods.java
+++ src/main/java/CartsMethods.java
@@ -3,7 +3,9 @@ import java.util.Map;
 public interface CartsMethods {
 
     void addItemsIntoCart(Product product, int quantity);
+
     double calculateFinalPrice();
 
+    public void displayStorage(Map<Product, Integer> storageMap);
 
 }
