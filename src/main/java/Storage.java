87069173dd2b5e142895b8670ce494886caa4920
Mateusz <wmateusz993@gmail.com>
2021-07-25 13:42:53 +0200

    Metoda remove inna

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,9 +1,9 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Storage implements RecordOfGoods {
-    public static  Map<Product, Integer> productStorage = new HashMap<>();
-public Product product;
+public class Storage  implements RecordOfGoods {
+    public Map<Product, Integer> productStorage = new HashMap<>();
+
     @Override
     public void putProductIntoStorage(Product product, int quantity) {
         if (productStorage.keySet().contains(product)) {
@@ -16,18 +16,21 @@ public Product product;
 
     @Override
     public void removeProductFromStorage(String name, int quantity) {
+        for(Product product: productStorage.keySet ()){
+          if(  product.getNameOfProduct ()==name){
 
-        if (productStorage.keySet().contains(product.getNameOfProduct ().equals (name))) {
             if (productStorage.get(product) - quantity < 0) {
                 System.out.println("Not enough items inside storage. Available products inside storage: " + productStorage.get(product));
-            } else {
-                productStorage.put(product, productStorage.get(product) - quantity);
+            } else {if(productStorage.get (product)-quantity==0){
+                productStorage.remove (product);
+            }else {
+                productStorage.put(product, productStorage.get(product) - quantity);}
             }
         } else {
             System.out.println("No product inside storage");
         }
         System.out.println(productStorage.size());
-    }
+    }}
 
     @Override
     public Map<Product, Integer> getStorage() {
