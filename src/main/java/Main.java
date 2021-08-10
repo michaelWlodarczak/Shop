929488e2d6969bedf3f551c4b0034da11d95ace1
Michal <michael.wlodarczak@gmail.com>
2021-08-10 20:42:50 +0200

    13th commit. Changes in menu, try-catch clause

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,60 +5,71 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            String menu = "Menu:\n" +
+                    "a.Add product to Shop\n" +
+                    "b.Remove product from Shop\n" +
+                    "c.Display list of products\n" +
+                    "d.Add product to receipt\n" +
+                    "e.Display receipt\n" +
+                    "f.EXIT";
+            final Scanner scanner = new Scanner(System.in);
+            Storage storage = new Storage();
+            Shop shop = new Shop(storage);
+            while (true) {
+                //int number;
+                String operation;
+                System.out.println();
+                System.out.println(menu);
+                //number = Integer.valueOf(scanner.nextLine());
+                operation =
+                        scanner.nextLine();
 
-
-        String menu = "Menu:\n" +
-                "1.Add product to Shop\n" +
-                "2.Remove product from Shop\n" +
-                "3.Display list of products\n" +
-                "4.Add product to recip\n" +
-                "5.Displey recip\n";
-        final Scanner scanner = new Scanner(System.in);
-        Storage storage = new Storage();
-        Shop shop = new Shop(storage);
-        while (true) {
-            int number;
-            System.out.println();
-            System.out.println(menu);
-            number = Integer.valueOf(scanner.nextLine());
-            switch (number) {
-                case 1:
-                    System.out.println("Enter product id");
-                    String id = scanner.nextLine();
-                    System.out.println("Enter name of Product");
-                    String name = scanner.nextLine();
-                    System.out.println("Enter price of product");
-                    Double price = Double.valueOf(scanner.nextLine());
-                    System.out.println("Enter type of product(GROCERIES, CHEMICALS, ELECTRONICS");
-                    String type = scanner.nextLine();
-                    Product prodcut = new Product(id, name, price, (ProductCategory.valueOf(type.toUpperCase(Locale.ROOT).trim())));
-                    System.out.println("Number of products");
-                    int count = Integer.valueOf(scanner.nextLine());
-                    storage.putProductIntoStorage(prodcut, count);
-
-                    break;
-                case 2:
-                    System.out.println("Enter product name ");
-                    String name1 = scanner.nextLine();
-                    System.out.println("How many products to remove");
-                    int num = Integer.valueOf(scanner.nextLine());
-                    storage.removeProductFromStorage(name1, num);
-                    break;
-                case 3:
-                    storage.displayStorage ();
-                    break;
-                case 4:
-                    System.out.println ("Name of product u want to add");
-                    String product = scanner.nextLine ();
-                    System.out.println ("Amount of products");
-                    int nums= Integer.valueOf (scanner.nextLine ());
-                    shop.addItemsIntoCart (product,nums);
-                    break;
-                case 5:
-                    System.out.println ("Your Recip:");
-                    shop.calculateFinalPrice ();
-                    break;
+                switch (operation) {
+                    case "a":
+                        System.out.println("Enter product id");
+                        String id = scanner.nextLine();
+                        System.out.println("Enter name of Product");
+                        String name = scanner.nextLine();
+                        System.out.println("Enter price of product");
+                        Double price = Double.valueOf(scanner.nextLine());
+                        System.out.println("Enter type of product(GROCERIES, CHEMICALS, ELECTRONICS");
+                        String type = scanner.nextLine();
+                        Product prodcut = new Product(id, name, price, (ProductCategory.valueOf(type.toUpperCase(Locale.ROOT).trim())));
+                        System.out.println("Number of products");
+                        int count = Integer.valueOf(scanner.nextLine());
+                        storage.putProductIntoStorage(prodcut, count);
+                        break;
+                    case "b":
+                        System.out.println("Enter product name ");
+                        String name1 = scanner.nextLine();
+                        System.out.println("How many products to remove");
+                        int num = Integer.valueOf(scanner.nextLine());
+                        storage.removeProductFromStorage(name1, num);
+                        break;
+                    case "c":
+                        storage.displayStorage();
+                        break;
+                    case "d":
+                        System.out.println("Name of product u want to add");
+                        String product = scanner.nextLine();
+                        System.out.println("Amount of products");
+                        int nums = Integer.valueOf(scanner.nextLine());
+                        shop.addItemsIntoCart(product, nums);
+                        break;
+                    case "e":
+                        System.out.println("Your Recip:");
+                        shop.calculateFinalPrice();
+                        break;
+                    case "f":
+                        System.out.println("EXIT");
+                        System.exit(-1);
+                    default:
+                        System.out.println("Invalid value");
+                }
             }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
     }
 }
